e479e24cd68c0316a5ee547d1a6e4a3360cefa8d
Thomas Coats <github@voodoolabs.net>
2018-10-18 23:51:27 +1300

    New version

--- README.md
+++ README.md
@@ -1,34 +1,9 @@
 # Odo Hub
-Simple pub sub.
-
-# Notes
-
-- Subscribers are required to callback to continue processing
-- Uses a template so events can also be logs e.g. `sent message to {recipient}` - recipient is replaced with the property from the event payload
-- Maintains a consistent execution order
+Simple pub sub with promises
 
 ```js
-var hub = require('odo-hub')();
+const hub = require('odo-hub')();
 
-var hellosub = hub.every('hello {person}', function(p, cb) {
-  console.log(p.person);
-  cb();
-});
-
-hub.once('hello {person}', function(p, cb) {
-  console.log('Once ' + p.person);
-  cb();
-});
-
-hub.all(function(e, description, p, cb) {
-  console.log(description);
-  cb();
-});
-
-hub.emit('hello {person}', { person: 'Frank' });
-hub.emit('hello {person}', { person: 'Bob' });
-hellosub.off();
-hub.emit('hello {person}', { person: 'Dave' }, function() {
-  console.log('Finished all events');
-});
+hub.on('cheese', type => console.log(type))
+hub.emit('cheese', 'brie')
 ```
--- index.js
+++ index.js
@@ -1,146 +1,29 @@
-const async = require('odo-async')
-const template = require('odo-template')
-
-module.exports = (defaultbindings) => {
+module.exports = (initial) => {
   const listeners = {}
-  const all = []
-  const none = []
+  const unhandled = []
+  for (let e of Object.keys(initial)) listeners[e] = [initial[e]]
+  const emit = (e, ...args) => {
+    if (listeners[e] == null) Promise.all(unhandled.map((fn) => fn(e, ...args)))
+    return Promise.all(listeners[e].map((fn) => fn(...args)))
+  }
 
-  const every = (e, cb) => {
-    if (listeners[e] == null) {
-      listeners[e] = []
-    }
-    listeners[e].push(cb)
-    return {
-      off: () => {
-        const index = listeners[e].indexOf(cb)
-        if (index !== -1) listeners[e].splice(index, 1)
-      }
-    }
+  return {
+    on: (e, fn) => {
+      if (!listeners[e]) listeners[e] = []
+      listeners[e].push(fn)
+    },
+    off: (e, fn) => {
+      if (!listeners[e]) return
+      const index = listeners[e].indexOf(fn)
+      if (index !== -1) listeners[e].splice(index, 1)
+    },
+    emit: emit,
+    unhandled: (fn) => unhandled.push(fn),
+    child: (initial) => {
+      const res = module.exports(initial)
+      res.unhandled((e, ...args) => emit(e, ...args))
+      return res
+    },
+    create: (initial) => module.exports(initial)
   }
-  const once = (e, cb) => {
-    const binding = every(e, (payload, callback) => {
-      binding.off()
-      cb(payload, callback)
-    })
-    return {
-      off: () => { binding.off() }
-    }
-  }
-  if (defaultbindings != null)
-    for (let events of defaultbindings)
-      every(events, defaultbindings[events])
-  const result = {}
-  result['new'] = (defaultbindings) => {
-    return module.exports(defaultbindings)
-  }
-  result.child = (defaultbindings) => {
-    const res = module.exports()
-    res.none((e, description, m, cb) => result.emit(e, m, cb))
-    if (defaultbindings != null)
-      for (events in defaultbindings)
-        res.every(events, defaultbindings[events])
-    return res
-  }
-  result.every = (events, cb) => {
-    if (!(events instanceof Array)) events = [events]
-    const bindings = events.map((e) => {
-      return {
-        event: e,
-        binding: every(e, cb)
-      }
-    })
-    return {
-      off: () => {
-        for (let e of bindings) e.binding.off()
-      }
-    }
-  }
-  result.once = (events, cb) => {
-    if (!(events instanceof Array)) events = [events]
-    const count = events.length
-    const bindings = events.map((e) => {
-      return {
-        event: e,
-        binding: once(e.event, (m, callback) => {
-          count--
-          if (count == 0)
-            cb(m, callback)
-          else
-            callback()
-        })
-      }
-    })
-
-    return {
-      off: () => {
-        for (let e of bindings) e.binding.off()
-      }
-    }
-  }
-  result.any = (events, cb) => {
-    const bindings = events.map((e) => {
-      return { event: e }
-    })
-
-    const unbind = () => {
-      for (let e of bindings) e.binding.off()
-    }
-
-    for (let e of bindings)
-      e.binding = once(e.event, () => {
-        unbind()
-        cb()
-      })
-
-    return { off: unbind }
-  }
-  result.all = (cb) => {
-    all.push(cb)
-    return {
-      off: () => {
-        const index = all.indexOf(cb)
-        if (index !== -1) all.splice(index, 1)
-      }
-    }
-  }
-  result.none = (cb) => {
-    none.push(cb)
-    return {
-      off: () => {
-        const index = none.indexOf(cb)
-        if (index !== -1) none.splice(index, 1)
-      }
-    }
-  }
-  result.emit = (e, m, ecb) => {
-    const description = template(e, m)
-    const tasks = []
-    for (let listener of all)
-      ((listener) => {
-        tasks.push((cb) => {
-          async.delay(() => { listener(e, description, m, cb) })
-        })
-      })(listener)
-
-    if (listeners[e] != null)
-      for (let listener of listeners[e].slice())
-        ((listener) => {
-          tasks.push((cb) => {
-            async.delay(() => { listener(m, cb) })
-          })
-        })(listener)
-    else
-      for (let listener of none)
-        ((listener) => {
-          tasks.push((cb) => {
-            async.delay(() => { listener(e, description, m, cb) })
-          })
-        })(listener)
-
-    async.parallel(tasks, () => {
-      if (ecb != null) ecb()
-    })
-  }
-  return result
 }
